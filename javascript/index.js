d7bd3ff07cb516df5d96c319462563e31afd15ef
Amairani <210300515@ucaribe.edu.mx>
2022-03-09 17:20:20 -0500

    Calculadora

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,31 @@
-console.log("Hola mundo")
+let operacion
 
+function calcular(operacion){
+   first = document.getElementById('primerNumero').value;
+   second = document.getElementById('segundoNumero').value;
+   let resultado = 0;
+ 
+if(!first){
+   alert("Agrega el primer numero.")
+   return 1;
+}
+   else if (!second){
+      alert("Agrega el segundo numero.")
+      return 1;
+}
+
+
+if(operacion == "suma"){
+   resultado = parseFloat(first)+parseFloat(second);
+   alert("La suma de los dos numeros es: " + resultado + ".");
+ }else if(operacion == "resta"){
+   resultado = parseFloat(first)-parseFloat(second);
+   alert("La resta de los dos numeros es: " + resultado + ".");
+ }else if(operacion == "multiplicación"){
+   resultado = parseFloat(first)*parseFloat(second);
+   alert("La multiplicacion de los dos numeros es: " + resultado + ".");
+ }else if(operacion == "división"){
+   resultado = parseFloat(first)/parseFloat(second);
+   alert("La division de los dos numeros es: " + resultado + ".");
+ }
+}
